14b05ebea070b5a14fc0ec8db4cb44b9b25eea9e
Willie Fatimehin <willie.fatimehin@gmail.com>
2019-03-13 22:59:14 -0400

    Starter Project

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Willie Fatimehin on 3/13/19.
+//  Copyright © 2019 Willie Fatimehin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
